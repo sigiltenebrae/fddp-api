071eb0fbb8a02d568632e18a45e8ea40131efd5a
sigiltenebrae <52510413+sigiltenebrae@users.noreply.github.com>
2026-06-09 20:28:44 -0400

    Refactor custom_cards.js to use ES6 modules

--- interfaces/custom_cards.js
+++ interfaces/custom_cards.js
@@ -1,15 +1,15 @@
-const config = require("../config/db.config.js");
-const {request, response} = require("express");
-const Pool = require('pg').Pool
+import "dotenv/config.js";
+import pg from "pg";
+const { Pool } = pg;
 const pool = new Pool({
-    user: config.USER,
-    host: config.HOST,
-    database: config.DB,
-    password: config.PASSWORD,
-    port: 5432,
+    "host":     process.env.POSTGRES_HOST,
+    "database": process.env.POSTGRES_DB,
+    "user":     process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "port":     5432,
 });
 
-exports.createCustomCard = (request, response) => {
+export function createCustomCard(request, response) {
     if (request.body && request.body.image && request.body.name) {
         const image = request.body.image;
         const name = request.body.name;
@@ -34,7 +34,7 @@ exports.createCustomCard = (request, response) => {
     }
 }
 
-exports.getCustomCards = (request, response) => {
+export function getCustomCards(request, response) {
     pool.query('SELECT * FROM custom_cards', (error, results) => {
         if (error) {
             console.log('Error getting custom cards');
@@ -45,7 +45,7 @@ exports.getCustomCards = (request, response) => {
     });
 }
 
-exports.deleteCustomCard = (request, response) => {
+export function deleteCustomCard(request, response) {
     const id = parseInt(request.params.id);
     pool.query('DELETE FROM custom_cards WHERE id = $1', [id],
         (error, results) => {
@@ -59,7 +59,7 @@ exports.deleteCustomCard = (request, response) => {
         })
 }
 
-exports.createCustomToken = (request, response) => {
+export function createCustomToken(request, response) {
     if (request.body && request.body.token) {
         const token = request.body.token;
         pool.query('INSERT INTO custom_tokens (name, image, type_line, power, toughness, oracle_text, w, u, b, r, g, creator) ' +
@@ -84,7 +84,7 @@ exports.createCustomToken = (request, response) => {
     }
 }
 
-exports.getCustomTokens = (request, response) => {
+export function getCustomTokens(request, response) {
     pool.query('SELECT * FROM custom_tokens', (error, results) => {
         if (error) {
             console.log('Error getting custom tokens');
@@ -95,7 +95,7 @@ exports.getCustomTokens = (request, response) => {
     });
 }
 
-exports.deleteCustomToken = (request, response) => {
+export function deleteCustomToken(request, response) {
     const id = parseInt(request.params.id);
     pool.query('DELETE FROM custom_tokens WHERE id = $1', [id],
         (error, results) => {
