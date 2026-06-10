b4eb34d2b76a2d30dffacde54150071eb115346b
sigiltenebrae <52510413+sigiltenebrae@users.noreply.github.com>
2026-06-09 20:24:13 -0400

    Update auth.js

--- interfaces/auth.js
+++ interfaces/auth.js
@@ -1,18 +1,21 @@
-const config = require("../config/db.config.js");
-const secret_config = require("../config/auth.config")
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const {Pool} = require("pg");
+import "dotenv/config.js";
+
+import * as jwt from "jsonwebtoken";
+import * as bcrypt = from "bcryptjs";
+
+import pg from "pg";
+
+const { Pool } = pg;
 
 const pool = new Pool({
-    user: config.USER,
-    host: config.HOST,
-    database: config.DB,
-    password: config.PASSWORD,
-    port: 5432,
+    "host":     process.env.POSTGRES_HOST,
+    "database": process.env.POSTGRES_DB,
+    "user":     process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "port":     5432,
 });
 
-exports.signup = (request, response) => {
+export function signup(request, response) {
     const username = request.body.username;
     const name = request.body.name;
     const password = bcrypt.hashSync(request.body.password, 8);
@@ -36,7 +39,7 @@ exports.signup = (request, response) => {
         });
 }
 
-exports.signin = (request, response) => {
+export function signin(request, response) {
     const username = request.body.username;
     pool.query('SELECT * FROM users WHERE username = $1', [username], (error, results) => {
         if (error) {
@@ -56,7 +59,7 @@ exports.signin = (request, response) => {
                 message: "Invalid Password!"
             });
         }
-        let token = jwt.sign({ id: user.id }, secret_config.secret, {
+        let token = jwt.sign({ id: user.id }, process.env.SECRET, {
             expiresIn: 86400 // 24 hours
         });
         pool.query('SELECT roles.name from roles left join user_roles on roles.id = user_roles."roleid" where user_roles."userid" = $1',
@@ -72,7 +75,7 @@ exports.signin = (request, response) => {
         });
 }
 
-exports.changepassword = (request, response) => {
+export function changepassword(request, response) {
     const id = request.body.id;
     pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
         if (error) {
