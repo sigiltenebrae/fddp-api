c48d3051678abc4e496080e799c7b66dcab0b74d
sigiltenebrae <52510413+sigiltenebrae@users.noreply.github.com>
2026-06-09 21:25:23 -0400

    Update themes.js

--- interfaces/themes.js
+++ interfaces/themes.js
@@ -1,15 +1,15 @@
-const config = require("../config/db.config.js");
-const scryfalldb = require('./scryfall');
-const Pool = require('pg').Pool
+import "dotenv/config.js";
+import pg from "pg";
+const { Pool } = pg;
 const pool = new Pool({
-    user: config.USER,
-    host: config.HOST,
-    database: config.DB,
-    password: config.PASSWORD,
-    port: 5432,
+    "host":     process.env.POSTGRES_HOST,
+    "database": process.env.POSTGRES_DB,
+    "user":     process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "port":     5432,
 });
 
-exports.getThemes = (request, response) => {
+export function getThemes(request, response) {
     pool.query('SELECT * FROM edhrec_themes', (theme_errors, theme_results) => {
         if (theme_errors) {
             return response.json({themes: [], tribes: []});
